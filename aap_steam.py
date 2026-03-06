17683f797efbce92d4b62892510e22c07543a966
Miguel Mota Cava <miguel.mota.cava@gmail.com>
2026-03-06 19:13:48 +0100

    cambios:streamlit

--- aap_steam.py
+++ aap_steam.py
@@ -8,7 +8,7 @@
 import streamlit as st
 
 
-st.title("Hello Streamlit-er 👋")
+st.title("Hello Streamlit-er")
 st.markdown(
     """ 
     This is a playground for you to try Streamlit and have fun. 
